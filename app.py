4d76ce7bb5176404608295a5cfa72efe4de1a570
Aref Ashrafi <aref.ashrafi97@gmail.com>
2023-05-02 21:19:37 +0200

    conitu

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask, send_file, request
-import cairosvg
 import io
+from PIL import Image
+import requests
 
 app = Flask(__name__)
 
-@app.route('/svg/png')
-def get_image():
+
+@app.route('/<f>/<t>')
+def get_image(f: str, t: str):
+    print(f, t)
     url = request.args.get('url')
-    print(url)
-    png_output = cairosvg.svg2png(url=url)
-    return send_file(io.BytesIO(png_output), mimetype='image/png')
+    if f == "svg" and t == "png":
+        import cairosvg
+        png_output = cairosvg.svg2png(url=url)
+        return send_file(io.BytesIO(png_output), mimetype='image/png')
+    if f == "jpeg" and t == "png":
+        png_output = Image.open(io.BytesIO(requests.get(url=url).content))
+        buffer = io.BytesIO()
+        png_output.save(buffer, 'PNG')
+        buffer.seek(0)
+        return send_file(buffer, mimetype='image/png')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
